07c2c2158d0d9f4c7726d33f53d11a303506a3d9
sumandeka223037 <159173969+sumandeka223037@users.noreply.github.com>
2024-09-25 11:17:20 +0530

    Time: 128 ms (18.01%), Space: 87.8 MB (14.66%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       // int ans;
-        unordered_set<int>ans;
-        for(int i:nums){
-            //ans=count(nums.begin(),nums.end(),i);
+        // int ans;
+        // sort(nums.begin(), nums.end());
+        // for(int i:nums){
+        //     ans=count(nums.begin(),nums.end(),i);
+        //     if(ans>1){
+        //         return i;
+        //     }
+        // }
+        // return 0;                  --------->only 54/59 testcases passed
+
+        //using hash map
+        unordered_set<int> ans;
+        for (int i : nums) {
             if (ans.count(i)) {
                 return i;
             }
             ans.insert(i);
-            }
+        }
         return -1;
     }
 };
