015a108ca79e9471f66c7cd5f88a703c5d860bc6
Suman Deka <159173969+sumandeka223037@users.noreply.github.com>
2024-10-29 10:18:13 +0530

    Time: 0 ms (100%), Space: 7.4 MB (50.09%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        long i = 1;
+        long int i = 1;
         while (i * i <= num){
             if (i * i == num){
                 return 1;
